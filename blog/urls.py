67cd596873e0bb44d40a41d4ef01e8e5df9a1b1c
Jay Parlar <parlar@gmail.com>
2013-04-05 23:02:51 -0400

    You can now edit and delete posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls import patterns, include, url
+from blog.models import Post
+from django.views.generic import TemplateView, ListView
+
 urlpatterns = patterns('blog.views',
     # Examples:
     # url(r'^$', 'toy_blog.views.home', name='home'),
@@ -6,9 +9,14 @@ urlpatterns = patterns('blog.views',
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    url(r'^$', 'index'),
+    url(r'^$', ListView.as_view(model=Post,
+                                context_object_name="post_list")),
     url(r'^cms/$', 'cms'),
     url(r'^cms/new/$', 'new_post'),
+    url(r'^cms/edit/(?P<slug>[-\w]+)/$', 'edit_post'),
+    url(r'^cms/delete/(?P<slug>[-\w]+)/$', 'delete_post'),
+    url(r'^cms/canceled/$', TemplateView.as_view(template_name='blog/canceled.html')),
+    url(r'^cms/deleted/$', TemplateView.as_view(template_name='blog/deleted.html')),
     url(r'^(?P<slug>[-\w]+)/$', 'detail'),
     # Uncomment the next line to enable the admin:
 )
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
 # Create your views here.
-from django.shortcuts import render_to_response, get_object_or_404
+from django.shortcuts import render, render_to_response, get_object_or_404
 from django.template import loader, RequestContext
 from blog.models import Post
+from blog.forms import PostForm
 
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 
 def index(request):
     latest_blog_list = Post.objects.all().order_by('-date')[:5]
@@ -13,10 +14,57 @@ def cms(request):
     return HttpResponse("Manage")
 
 def new_post(request):
-    if request.POST:
-        print "HAVE POST", request.POST
-    return render_to_response('blog/new_post.html', {}, context_instance = RequestContext(request))
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data['title']
+            body = form.cleaned_data['body']
+            slug = form.cleaned_data['slug']
+            date = form.cleaned_data['date']
+            post = Post.objects.create(title=title, body=body, slug=slug, date=date)
+            return HttpResponseRedirect('/%s/' % slug)
+        else:
+            print "Bad form data"
+    else:
+        form = PostForm()
+    return render(request, 'blog/new_post.html', {'form':form, 'action':'/cms/new/'}) 
 
+def make_post_data(post):
+    return post_data
+
+def edit_post(request, slug):
+    print request.user
+    post = get_object_or_404(Post, slug=slug)
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post.title = form.cleaned_data['title']
+            post.body = form.cleaned_data['body']
+            post.slug = form.cleaned_data['slug']
+            post.date = form.cleaned_data['date']
+            post.save()
+            return HttpResponseRedirect('/%s/' % slug)
+    else:
+        post_data = {'title': post.title,
+                     'body': post.body,
+                     'slug': post.slug,
+                     'date': post.date
+                     }
+        form = PostForm(initial = post_data)
+    return render(request, 'blog/new_post.html', {'form':form, 'title':'Edit Post', 'action':'/cms/edit/%s/'%slug})
+
+def delete_post(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    if request.method == 'POST':
+        print request.POST
+        if '_cancel' in request.POST:
+            print "Cancelled"
+            return HttpResponseRedirect('/cms/canceled')
+        elif '_delete' in request.POST:
+            print "Deleted"
+            post.delete()
+            return HttpResponseRedirect('/cms/deleted')
+    return render(request, 'blog/delete_post.html', {'post':post, 'path':request.get_full_path})
 
 def detail(request, slug):
     p = get_object_or_404(Post, slug=slug)
